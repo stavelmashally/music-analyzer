4f81a220b1440c26291d8fca5ffea50cdd819e54
Stav Elmashally <stavelmashally6@gmail.com>
2020-09-15 11:19:21 +0300

    express static middleware

--- app.js
+++ app.js
@@ -9,6 +9,17 @@ app.use(bodyParser.json());
 
 require('./routes')(app);
 
+if (process.env.NODE_ENV === 'production') {
+  // Serve up production assets
+  app.use(express.static('client/build'));
+
+  // Serve up the index.html
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, async () => {
